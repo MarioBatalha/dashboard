3e1b28e9576a5eaa76479c3679e19087710131c3
MarioBatalha <mariobatalha9@gmail.com>
2021-10-17 15:43:35 +0100

    add reducer

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import './style.css';
 import Car from '../../asset/img/car.png';
 import House from '../../asset/img/house.png';
@@ -7,6 +8,30 @@ import Footer from '../../components/Footer';
 import {Link} from 'react-router-dom';
 
 const Landing = () => {
+  //const [plan, setPlan] = useState([carPlan, housePlan, travelPlan]);
+  const [carPlan, setCarPlan] = useState(false);
+  const [housePlan, setHousePlan] = useState(false);
+  const [travelPlan, setTravelPlan] = useState(false);
+
+  const handleReducer = (state, action) => {
+    switch (action.type) {
+      case carPlan:
+        setCarPlan(true);
+        alert('Selecionaste o plano de compra de casa');
+        break;
+      case housePlan:
+        setHousePlan(true);
+        alert('Selecionaste o plano de compra de casa');
+        break;
+      case travelPlan:
+        setTravelPlan(true);
+        alert('Selecionaste o plano de compra de casa');
+        break;
+      default:
+        alert('O plano escolhido não existe');
+    }
+  };
+
   return (
     <>
       <nav>
@@ -26,12 +51,10 @@ const Landing = () => {
       </nav>
       <main>
         <h1>
-          Compre a sua casa ou carro 
+          Compre a sua casa ou carro
           <br /> com os nosso planos.
         </h1>
-        <p>
-          Escolha um plano, subescreva e desfrute dos benefícios.
-        </p>
+        <p>Escolha um plano, subescreva e desfrute dos benefícios.</p>
         <div className="plan">
           <div className="plan__item">
             <span>
@@ -41,7 +64,7 @@ const Landing = () => {
             <p className="item"> * Pagamento parcelado</p>
             <p className="item"> * Melhor custo beneficio</p>
             <p className="item"> * Pagou a primeira - Levou o producto</p>
-            <button className="subescrever">subescrever</button>
+            <button className="subescrever" /* onClick={handleReducer(plan, e => setPlan(carPlan))} */>subescrever</button>
           </div>
           <div className="plan__item">
             <span>
